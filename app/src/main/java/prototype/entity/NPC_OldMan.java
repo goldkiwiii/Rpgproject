488f093083ea545eb27b9671e3d219f3bb4b910c
kangho <burgerking@handong.ac.kr>
2024-12-03 16:45:39 +0900

    kangho's update

--- app/src/main/java/prototype/entity/NPC_OldMan.java
+++ app/src/main/java/prototype/entity/NPC_OldMan.java
@@ -14,8 +14,19 @@ public class NPC_OldMan extends Entity {
         super(gp);
         direction = "down";
         speed = 1;
+        setDialogue();
         getImage();
     }
+    public void setDialogue(){
+        dialogue[0] = "Hello, lad.";
+        dialogue[1] = "So, You narrived HGU";
+        dialogue[2] = "You are student now ";
+        dialogue[3] = "Find Bag!";
+
+
+    }
+
+
 
     public void getImage() {
         //load player image in res file
@@ -28,6 +39,13 @@ public class NPC_OldMan extends Entity {
         right1 = setup("/npc/oldman_right_1");
         right2 = setup("/npc/oldman_right_2");
     }
+
+    @Override
+    public void speak() {
+        //do this character specific dialogue
+        super.speak();
+    }
+
     public void setAction(){
         actionLockCounter ++;
         if (actionLockCounter == 120 ) {
@@ -47,4 +65,6 @@ public class NPC_OldMan extends Entity {
             }
         actionLockCounter = 0;
     }
-}}
+}
+
+}
